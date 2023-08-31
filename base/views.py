b8c41e857b759e2e2d5a643b59ee42992583c81d
SahinS9 <shahinsaatov@gmail.com>
2023-08-31 15:10:11 +0400

    user registration - register page or login page, usage of page name to understand which div is going to filtered in the html. Hello message to the user after registration

--- base/views.py
+++ base/views.py
@@ -13,6 +13,10 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
+#for registration form
+from django.contrib.auth.forms import UserCreationForm
+
+
 from .forms import RoomForm
 
 # rooms = [
@@ -24,11 +28,14 @@ from .forms import RoomForm
 #have function login - it s method so itll create problem
 def LoginPage(request):
 
+    page = 'login'
+
     if request.user.is_authenticated:
         return redirect ('home')
 
     if request.POST:
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
+        
         password = request.POST.get('password')
 
         try:
@@ -44,13 +51,39 @@ def LoginPage(request):
             return redirect ('home')
         else:
             messages.error(request,'Username or password does not exist')
-    context = {}
+    context = {'page':page}
     return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    page = 'register'
+    form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            #it will save user data but will not commit it to the db so we will lowercase username in order not to face with problem
+            user = form.save(commit =False)
+            user.username = user.username.lower() #make it lowercase
+            user.save()    
+
+            #login user directly after registration 
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration')
+
+
+        
+
+
+    context = {'page':page,'form':form}
+    return render(request,'base/login_register.html',context)
+
 
 
 def home(request):
